3ed13e71f94940c85e5f6fbf9431637310476437
max <max@max-macbook-pro.wireless-1x.unc.edu>
2019-08-27 00:53:21 -0400

    Completed A1Adept

--- src/a1/A1Adept.java
+++ src/a1/A1Adept.java
@@ -6,9 +6,149 @@ public class A1Adept {
 
 	public static void main(String[] args) {
 		
+		Customer[] customers = getCustomersFromUserInput();
+		printShoppingInfo(customers);
+	}
+	
+	public static Customer[] getCustomersFromUserInput() {
 		Scanner scan = new Scanner(System.in);
 
-		// Your code follows here.
+		// Read in count of items in store to process
+		int numberOfItemsInStore = scan.nextInt();
 		
+		// Create array of Item class to store items
+		Item[] items = new Item[numberOfItemsInStore];
+		
+		// Read item information into array
+		for (int i = 0; i < items.length; i++) {
+			String name = scan.next();
+			double price = scan.nextDouble();
+			
+			items[i] = new Item(name, price);
+		}
+		
+		// Read in count of customers to process
+		int numberOfCustomers = scan.nextInt();
+		
+		// Create array of Customer class to store customers
+		Customer[] customers = new Customer[numberOfCustomers];
+		
+		// Read customers information into array
+		for (int i = 0; i < customers.length; i++) {
+			String firstName = scan.next();
+			String lastName = scan.next();
+			
+			int numItemsBought = scan.nextInt();
+			
+			// Create array of Item to store items for current customer
+			Item[] itemsBought = new Item[numItemsBought];
+			
+			// Read item information into array
+			for (int j = 0; j < itemsBought.length; j++) {
+				int quantity = scan.nextInt();
+				String name = scan.next();
+				
+				// Find item from items array and add it to current customer's bought items in correct quantity
+				for (Item item : items) {					
+					if (item.name.equals(name)) {
+						itemsBought[j] = new Item(item);
+						itemsBought[j].quantity = quantity;
+						break;
+					}
+				}
+			}
+			
+			// Create customer with processed information
+			customers[i] = new Customer(firstName, lastName, itemsBought);
+		}
+		
+		// All input parsed, so close scanner
+		scan.close();
+		
+		// Return processed information
+		return customers;
 	}
+	
+	/* printShoppingInfo() 
+	 * Prints shopping information from array of Customer
+	 */
+	public static void printShoppingInfo(Customer[] customers) {
+		String biggestString = "Biggest: ";
+		String smallestString = "Smallest: ";
+		
+		double max = 0.0;
+		double min = 0.0;
+		
+		double totalMoneySpent = 0.0;
+		
+		// Add customers information to info String
+		for (int i = 0; i < customers.length; i++) {
+			double total = 0;
+			
+			// Calculate total for current customer
+			for (Item j : customers[i].itemsBought) {
+				total += j.quantity * j.price;
+			}
+			
+			// Set biggest and smallest to first customer's total
+			if (i == 0) {
+				max = total;
+				min = total;
+			}
+			
+			// Add current customer's total to totalMoneySpent
+			totalMoneySpent += total;
+			
+			// Check if total is new Max
+			if (total > max) {
+				// Change biggestString accordingly
+				biggestString = "Biggest: " + customers[i].firstName + " " + customers[i].lastName + " (" + String.format("%.2f", total) + ")";
+				max = total;
+			}
+			
+			// Check if total is new Min
+			if (total < min) {
+				// Change smallestString accordingly
+				smallestString = "Smallest: " + customers[i].firstName + " " + customers[i].lastName + " (" + String.format("%.2f", total) + ")";
+				min = total;
+			}
+		}
+		
+		// Calculate average money spent
+		double average = totalMoneySpent / customers.length;
+		
+		// Print info String
+		System.out.println(biggestString + "\n" + smallestString + "\n" + "Average: " + String.format("%.2f", average));
+	}
+	
+		// Nested helper class
+		public static class Customer {
+			String firstName;
+			String lastName;
+			Item[] itemsBought;
+			
+			public Customer(String firstName, String lastName, Item[] itemsBought) {
+				this.firstName = firstName;
+				this.lastName = lastName;
+				this.itemsBought = itemsBought;
+			}
+		}
+		
+		// Nested helper class
+		public static class Item {
+			int quantity;
+			String name;
+			double price;
+			
+			public Item(String name, double price) {
+				this.name = name;
+				this.price = price;
+			}
+			
+			// Copy constructor
+			public Item(Item item) {
+				this.name = item.name;
+				this.price = item.price;
+			}
+		}
 }
